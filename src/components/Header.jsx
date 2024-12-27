6c6354086ba66a395a674f49ad9364a90cdbc8f8
yipjasmi <yipjasmi@usc.edu>
2024-12-26 22:26:51 -0800

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,38 @@
+import './Header.css';
+import logo from '../assets/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faSlack } from '@fortawesome/free-brands-svg-icons';
+
 const Header = () => {
     return (
-        <div>
-            <h1>Hello world.</h1>
-        </div>
-    )
-}
+        <header className="header">
+            <div className="logo">
+                <img src={logo} alt="Club Logo" />
+            </div>
+
+            <nav className="nav">
+                <a href="#">Our Values</a>
+                <a href="#">Practice</a>
+                <a href="#">Trips</a>
+                <a href="#">Comp Team</a>
+                <a href="#">DEI Initiatives</a>
+                <a href="#">Info</a>
+                <a href="#">Resources</a>
+            </nav>
+
+            <div className="socials">
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faFacebookF} />
+                </a>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="https://slack.com" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faSlack} />
+                </a>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
