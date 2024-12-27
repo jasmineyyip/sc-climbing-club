b3b0a99075726101989587b2de5c5fed4140764e
yipjasmi <yipjasmi@usc.edu>
2024-12-26 21:55:15 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the SC Rock Climbing Club!</h1>
+        </div>
+    );
+};
+
+export default HomePage;
